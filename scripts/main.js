afaf0be3b543279343da86453ccb2a6a07809452
Wiktor Moskwa <49322534+MrSaper112@users.noreply.github.com>
2022-02-07 12:19:32 +0100

    Delete scripts directory

--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-import { PlaneManager } from "./GeneratePlane.js";
-const plane = document.getElementById("plane");
-let planeManager = new PlaneManager(plane);
-window.addEventListener("DOMContentLoaded", () => {
-    console.log("Dupa");
-    planeManager.generatePlane();
-});
